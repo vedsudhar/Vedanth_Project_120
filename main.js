c5041861c7c8b8a2cb46d778763a8d728c79cff2
vedsudhar <122882485+vedsudhar@users.noreply.github.com>
2023-08-01 17:11:48 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+//handgesture_1 = "" ;
+//handgesture_2 = "" ; 
+
+
+Webcam.set({
+    width:350,
+    height:300,
+    image_format : 'png',
+    png_quality:90
+});
+
+camera = document.getElementById("camera");
+
+Webcam.attach('#camera');
+
+//https://teachablemachine.withgoogle.com/models/gYoN2-G4i/
+
+function take_snapshot()
+{
+    Webcam.snap(function(data_uri) {
+        document.getElementById("result").innerHTML =
+        '<img id="captured_image" src="'+data_uri+'"/>';
+    });
+
+    console.log('ml5 version', ml5.version);
+
+    classifier = ml5.imageClassifier('https://teachablemachine.withgoogle.com/models/gYoN2-G4i/model.json',modelLoaded);
+
+    function modelLoaded() {
+        console.log('Model Loaded!');
+    }
+}
